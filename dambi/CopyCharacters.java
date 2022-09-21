5f46a1c9841e65da8b029abfab25468797cdc218
Karmele Elorza <96118700+KaR21@users.noreply.github.com>
2022-09-21 12:16:55 +0200

    Add files via upload

---
+++ dambi/CopyCharacters.java
@@ -0,0 +1,33 @@
+/** Programa honek proiektuaren erroan dagoen Xanadu fitxategia karakterez karaktere irakurtzen du, 
+    * karaktere bakoitza characteroutput.txt fitxategian idatziz.
+*/
+
+package dambi;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyCharacters {
+    public static void main(String[] args) throws IOException {
+
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("./xanadu.txt");
+            outputStream = new FileWriter("characteroutput.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                    outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
