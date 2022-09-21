09edc37c2c44d703babc49f08f4f735d7ed9bb29
Karmele Elorza <96118700+KaR21@users.noreply.github.com>
2022-09-21 12:19:15 +0200

    Add files via upload

---
+++ Ariketak/1-Fitxategiak_StreamModuan/demo/src/main/java/dambi/Root.java
@@ -0,0 +1,20 @@
+/** Programa honek i aldagaian gordeta dagoen zenbakiaren (Kasu honetan 2 zenbakia) erro karratua kalkulatu eta erakusten du.
+*/
+package dambi;
+
+public class Root {
+    public static void main(String[] args) {
+        int i = 2;
+        double r = Math.sqrt(i);
+        
+        System.out.print("The square root of ");
+        System.out.print(i);
+        System.out.print(" is ");
+        System.out.print(r);
+        System.out.println(".");
+
+        i = 5;
+        r = Math.sqrt(i);
+        System.out.println("The square root of " + i + " is " + r + ".");
+    }
+}
