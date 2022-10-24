22d572d6efbc3a9bd9a587c98f787002321b6844
Karmele Elorza <96118700+KarmeleElorza@users.noreply.github.com>
2022-10-24 12:42:17 +0200

    Delete Ariketak directory

--- Ariketak/1-Fitxategiak_StreamModuan/demo/src/main/java/dambi/Format.java
+++
@@ -1,9 +0,0 @@
-/** Programa honek Math.PI(konstantea pi zenbakia gordetzen duena) dagoen zenbakia erakusten du formatu berezian.
-*/
-package dambi;
-
-public class Format {
-    public static void main(String[] args) {
-        System.out.format("%f, %1$+020.10f %n", Math.PI);
-    }
-}
